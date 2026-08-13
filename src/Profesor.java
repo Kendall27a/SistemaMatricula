afdbbce219fa7a185ebae670dded3999a2dea54d
Keanny Obando <keobandobo@est.utn.ac.cr>
2026-08-13 14:37:32 -0600

    Se hizo la clase profesor

---
+++ src/Profesor.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author keann
+ */
+public class Profesor {
+
+    private int id;
+    private String nombre;
+    private String cedula;
+    private String especialidad;
+
+    public Profesor() {
+    }
+
+    public Profesor(int id, String nombre, String cedula, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
